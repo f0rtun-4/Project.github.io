89e09a196ca932b84d1ce8c4bafbd0d08397ed49
f0rtun-4 <sangadpintar@gmail.com>
2022-07-10 21:47:50 +0800

    del useles syntax, add new css

--- javascript.js
+++ javascript.js
@@ -40,41 +40,4 @@ $(document).scroll(function () {
   }
 });
 
-//countdown 
 
-const countdown = () => {
-  // Specify the date and time we are counting down to.
-  const countDate = new Date("Dec 26, 2022 00:00:00").getTime();
-  const now = new Date().getTime();
-  const remainingTime = countDate - now;
-
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const textDay = Math.floor(remainingTime / day);
-  const textHour = Math.floor((remainingTime % day) / hour);
-  const textMinute = Math.floor((remainingTime % hour) / minute);
-  const textSecond = Math.floor((remainingTime % minute) / second);
-
-  document.querySelector(".day").innerText = textDay > 0 ? textDay : 0;
-  document.querySelector(".hour").innerText = textHour > 0 ? textHour : 0;
-  document.querySelector(".minute").innerText = textMinute > 0 ? textMinute : 0;
-  document.querySelector(".second").innerText = textSecond > 0 ? textSecond : 0;
-};
-
-// should use 500 as setInterval won't always run on time.
-setInterval(countdown, 500);
-
-// jquery script
-
-const SectionOneHide = document.querySelector(".section-1")
-const SectionTwoHide = document.querySelector(".section-2")
-const show = document.querySelector(".container-2");
-
-function reveal() {
-  SectionOneHide.style.display = "none"
-  SectionTwoHide.style.display = "none"
-  show.style.display = "block"
-}
